bee972465013856b428742cd4b66ecf3b42d5b45
showiproute <m18752095913@163.com>
2021-08-08 01:17:50 +0800

    hash

--- src/hash1743.java
+++ src/hash1743.java
@@ -5,63 +5,42 @@ public class hash1743 {
     //输入：adjacentPairs = [[4,-2],[1,4],[-3,1]]
     //输出：[-2,4,1,-3]
     public int[] restoreArray(int[][] adjacentPairs) {
-        HashMap<Integer,ArrayList<Integer>> hashMap = new HashMap<>();
-        for(int i =0 ;i<adjacentPairs.length;i++){
-            hashMap.putIfAbsent(adjacentPairs[i][0],new ArrayList<Integer>());
-            hashMap.putIfAbsent(adjacentPairs[i][1],new ArrayList<Integer>());
+        if(adjacentPairs.length == 1) return adjacentPairs[0];
+        HashMap<Integer, ArrayList<Integer>> hashMap = new HashMap<>();
+        for(int i = 0;i<adjacentPairs.length;i++){
+            hashMap.putIfAbsent(adjacentPairs[i][0],new ArrayList<>());
+            hashMap.putIfAbsent(adjacentPairs[i][1],new ArrayList<>());
             hashMap.get(adjacentPairs[i][0]).add(adjacentPairs[i][1]);
             hashMap.get(adjacentPairs[i][1]).add(adjacentPairs[i][0]);
         }
-        int first = -1;
-        for(Map.Entry<Integer,ArrayList<Integer>> keyVal : hashMap.entrySet()) {
+        int[] rst = new int[adjacentPairs.length+1];
+        for(Map.Entry<Integer, ArrayList<Integer>> keyVal : hashMap.entrySet()) {
             if(keyVal.getValue().size() == 1) {
-                first = keyVal.getKey();
+                rst[0] = keyVal.getKey();
                 break;
             }
         }
-
-        int[] rst = new int[adjacentPairs.length+1];
-        rst[0] = first;
-        rst[1] = hashMap.get(first).get(0);
+        rst[1] = hashMap.get(rst[0]).get(0);
         for(int i = 2;i<rst.length;i++){
-            int key = rst[i - 1];
-            ArrayList<Integer> list = hashMap.get(key);
-            for(int j = 0;j<list.size();j++){
-                if(list.get(j) != rst[i-2]) {
-                    rst[i] = list.get(j);
-                    break;
+            ArrayList<Integer> arrayList = hashMap.get(rst[i-1]);
+            for(int j = 0;j<arrayList.size();j++){
+                if(arrayList.get(j) != rst[i-2]) {
+                    rst[i] = arrayList.get(j);
                 }
             }
         }
 
-       return rst;
+        return rst;
     }
 
-//    public void dfs(HashMap<Integer,ArrayList<Integer>> hashMap,ArrayList<Integer> temp,int first) {
-//        /*
-//            4  | -2 , 1
-//    -2 | 4
-//    1  | 4 , -3
-//    -3 | 1
-//         */
-//        if(temp.contains(first)) return;
-//        temp.add(first);
-//        ArrayList<Integer> arrayList = hashMap.get(first);
-//        for (Integer integer : arrayList) {
-//            if (temp.contains(integer)) continue;
-//            dfs(hashMap, temp, integer);
-//        }
-//
-//    }
-
     public static void main(String[] args) {
-        int[][] adj = {
+        hash1743 obj = new hash1743();
+        int[][] arr = {
                 {4,-2},
                 {1,4},
                 {-3,1}
         };
-        hash1743 obj = new hash1743();
-        int[] ints = obj.restoreArray(adj);
+        int[] ints = obj.restoreArray(arr);
         System.out.println(Arrays.toString(ints));
     }
 }
