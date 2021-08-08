ce9756589f8f9bc258e147c4ef008dc0c6fe8e27
showiproute <m18752095913@163.com>
2021-08-08 22:49:55 +0800

    dfs 3 colors

--- src/dfs802.java
+++ src/dfs802.java
@@ -1,38 +1,37 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
-public class top802 {
+public class dfs802 {
 
+    //输入：graph = [[1,2],[2,3],[5],[0],[5],[],[]]
+    //输出：[2,4,5,6]
     public List<Integer> eventualSafeNodes(int[][] graph) {
         List<Integer> rst = new ArrayList<>();
+        int[] color = new int[graph.length];
         for(int i = 0;i<graph.length;i++){
-            if(graph[i].length == 0) {
-                rst.add(i);
-                continue;
-            }
-            HashSet<Integer> visited = new HashSet<>();
-            if(dfs(i,graph,visited)){
+            if(dfs(graph,color,i)){
                 rst.add(i);
             }
         }
-
         return rst;
     }
 
-    public boolean dfs(int src,int [][]graph,HashSet<Integer> visited) {
-        if(visited.contains(src)) return false;
-        for(int i = 0;i<graph[i].length;i++){
-            int val = graph[src][i];
-            visited.add(val);
-            if(dfs(val,graph,visited)){
-                return true;
-            }
-            visited.remove(val);
+    public boolean dfs(int[][] graph,int[] color,int index) {
+        if(color[index] != 0) {
+            return color[index] == 2;
         }
-
+        color[index] = 1;
+        for(int i = 0;i<graph[index].length;i++){
+            if(!dfs(graph,color,graph[index][i])) {
+                return false;
+            }
+        }
+        color[index] = 2;
         return true;
     }
+
 
     //输入：graph = [[1,2],[2,3],[5],[0],[5],[],[]]
     //输出：[2,4,5,6]
@@ -46,7 +45,7 @@ public class top802 {
                 {},
                 {}
         };
-        top802 obj = new top802();
+        dfs802 obj = new dfs802();
         List<Integer> integers = obj.eventualSafeNodes(arr);
         System.out.println(integers.toString());
     }
